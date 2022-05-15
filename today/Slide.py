0032e1d7d507c2338a7d0b0d770482549e8ba1ca
kun <369319633@qq.com>
2022-05-15 13:37:08 +0800

    sorted

--- today/Slide.py
+++ today/Slide.py
@@ -1,3 +1,7 @@
 # 列表的切片操作
-list1 = ['热巴', '冬妮娅', '杨幂', 'Java', 'Python', 100, 99]
+list1 = ['热巴', '冬妮娅', '杨幂', 'Java', 'Python', "100", "99"]
+print(list1)
+newlist = sorted(list1)
+print(newlist)
+newlist = sorted(list1, reverse=True)
 print(list1)
