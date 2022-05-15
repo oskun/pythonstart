f6bec1e5722844ca80656e08e0709cba9405de6a
kun <369319633@qq.com>
2022-05-15 11:16:02 +0800

    tianjia

--- Hello.py
+++ Hello.py
@@ -1,7 +1,14 @@
-def fib(n):
-    a, b = 0, 1
-    while a < n:
-        print(a, b, end=' ')
-        a, b = b, a + b
-    print()
-    
+brands = ['mac', 'IBM', "TCL", 'lenovo', 'acer', 'amd']
+indexs = []
+len = len(brands) - 1
+for index in range(len):
+    if ("mac" in brands[index] or "amd" in brands[index]):
+        indexs.append(index)
+# 这里犯错误，index 已经改变
+for index in indexs:
+    del brands[index]
+
+
+# 判断在列表中
+# test git
+print(brands)
